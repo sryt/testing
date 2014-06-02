2dd3c1b502eacf8a616af2bb3c6fcf1fe0199025
DEll <DEll@DEll-PC>
2014-06-02 15:13:34 +0530

    Initial check in of test

---
+++ TestRepository/src/com/test/MainTesting.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class MainTesting {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hi from java class");
+		OtherTesting ot = new OtherTesting();
+		ot.display();
+	}
+
+}
---
+++ TestRepository/src/com/test/OtherTesting.java
@@ -0,0 +1,8 @@
+package com.test;
+
+public class OtherTesting {
+
+	public void display(){
+		System.out.println("Displaying from the other class other method...............");
+	}
+}
